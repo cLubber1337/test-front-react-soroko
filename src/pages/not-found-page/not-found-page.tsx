06e378e37e4dccec8a93eda7b4f6dc3ddc41bffd
Andrey Soroko <supersoroko@gmail.com>
2023-11-08 03:15:10 +0300

    feat: not found page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom'
+import { UiButton } from '@/components/ui-kit'
+import s from './not-found-page.module.scss'
+import imag404 from '../../../public/404.svg'
 
 export const NotFoundPage = () => {
   return (
     <div>
-      <h1>404</h1>
-      <Link to="/">Go to main page</Link>
+      <div className={s.wrapper}>
+        <img src={imag404} alt="404" className={s.image} />
+
+        <UiButton as={Link} to="/" className={s.button}>
+          Go to main page
+        </UiButton>
+      </div>
     </div>
   )
 }
